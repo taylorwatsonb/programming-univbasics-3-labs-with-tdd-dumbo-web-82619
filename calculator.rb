8436ff9af5e21f3e1f70eb5226828a466eb5a366
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-01 18:11:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,8 @@
  second_number = 0
  
  sum = first_number + second_number
- 
- sum
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
  
